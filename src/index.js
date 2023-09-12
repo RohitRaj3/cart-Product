8a9e4992d3a4d8026ce653dfda88bc4d2b5b46c8
Rohit Raj <rs1977@srmist.edu.in>
2023-09-12 16:05:28 +0530

    hosting

--- src/index.js
+++ src/index.js
@@ -2,16 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import * as firebase from 'firebase';
+// import 'firebase/firestore';
+import firebase from 'firebase/compat/app';
+import 'firebase/firestore';
+import {} from 'firebase/compat/firestore';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB5x3GxP0C79W3FIpoxgFuLo6Qm2xcnbZ8",
+  authDomain: "cart-21281.firebaseapp.com",
+  projectId: "cart-21281",
+  storageBucket: "cart-21281.appspot.com",
+  messagingSenderId: "733397031813",
+  appId: "1:733397031813:web:925474866842bd5add82ec"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+
+
+// import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+// reportWebVitals();
